6c70f9ce0fcc7ec42a8ff2dbd7c5d6d8c61c25cc
liverHawk <hawkyofkn5365@gmail.com>
2025-07-25 20:53:34 +0900

    dvc: commit experiment 9a30986f4dfcd27c853c1576247d0d1ba510b077e68b1526049f313bb3582502

--- src/evaluate.py
+++ src/evaluate.py
@@ -9,7 +9,7 @@ from dvclive import Live
 import sklearn.metrics as metrics
 
 
-def save_evaluation_results(live: Live, predict_probs, test_labels):
+def save_evaluation_results(live: Live, predict_probs, test_labels: pd.Series):
     predict_actions = [prop.argmax() for prop in predict_probs]
 
     # predict_probs -> predict_actions vs test_label
@@ -29,12 +29,23 @@ def save_evaluation_results(live: Live, predict_probs, test_labels):
         predict_actions,
         title="Confusion Matrix",
     )
+    conf_matrix = metrics.confusion_matrix(test_labels, predict_actions)
+
+    txt_path = os.path.join("evaluate", "confusion_matrix.csv")
+    with open(txt_path, "w") as f:
+        length = len(test_labels.unique())
+        f.write(",".join([str(i) for i in range(length)]))
+        f.write("\n")
+        for index, row in enumerate(conf_matrix):
+            f.write(f"{index},")
+            f.write(",".join(str(i) for i in row))
+            f.write("\n")
 
     import matplotlib.pyplot as plt
     import seaborn as sns
-    conf_matrix = metrics.confusion_matrix(test_labels, predict_actions, normalize='true')
+    conf_matrix_normalize = metrics.confusion_matrix(test_labels, predict_actions, normalize='true')
     fig, ax = plt.subplots()
-    sns.heatmap(conf_matrix, annot=False, cmap='Blues', ax=ax)
+    sns.heatmap(conf_matrix_normalize, cmap="YlGn", ax=ax, linewidths=.5, linecolor='gray')
     ax.set_title("Confusion Matrix")
     ax.set_xlabel("Predicted")
     ax.set_ylabel("Actual")
